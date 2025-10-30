d69878f023182578aba0119469775a9bfcffcfae
Raza-Ur-Rehman <mrazaurrehman12345@gmail.com>
2025-10-31 02:09:16 +0500

    update code

--- src/utils/Work.js
+++ src/utils/Work.js
@@ -2,32 +2,32 @@ export const works = [
   {
     imgSrc: '/images/project-1.jpg',
     title: 'QR Code Generator',
-    tags: ['API', 'MVC', 'Development'],
+    tags: ['API', 'JS', 'Development'],
     projectLink: 'https://raza-ur-rehman.github.io/QR-code-Generator/'
   },
   {
     imgSrc: '/images/project-2.jpg',
-    title: 'QR Code Generator',
-    tags: ['API', 'SPA'],
-    projectLink: 'https://raza-ur-rehman.github.io/QR-code-Generator/'
+    title: 'Password Generator',
+    tags: ['Development',"JS"],
+    projectLink: 'https://raza-ur-rehman.github.io/random-password-generator/'
   },
   {
     imgSrc: '/images/project-3.jpg',
-    title: 'Recipe app',
-    tags: ['Development', 'API'],
-    projectLink: ''
+    title: 'Weather App',
+    tags: ['Development', 'API' ,"JS"],
+    projectLink: 'https://weatherapp9.vercel.app/'
   },
   {
     imgSrc: '/images/project-4.jpg',
-    title: 'Real state website',
+    title: 'E-commerce website',
     tags: ['Web-design', 'Development'],
-    projectLink: 'https://github.com/codewithsadee-org/wealthome'
+    projectLink: 'https://chawk-bazar-trendy.netlify.app/'
   },
   {
     imgSrc: '/images/project-5.jpg',
-    title: 'eCommerce website',
-    tags: ['eCommerce', 'Development'],
-    projectLink: 'https://github.com/codewithsadee/anon-ecommerce-website'
+    title: 'Daily Blogging App',
+    tags: ['BOLG',"Firebase","JS", 'Development'],
+    projectLink: 'https://daily-bloging.vercel.app/'
   },
   {
     imgSrc: '/images/project-6.jpg',
